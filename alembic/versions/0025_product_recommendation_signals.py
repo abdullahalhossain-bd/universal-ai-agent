89460e72b6c9389a87e30dbd8cc9d204413f1227
abdullahalhossain-bd <shahanaj7719@gmail.com>
2026-09-09 11:33:41 +0600

    Fix Alembic revision ID length for PostgreSQL

--- alembic/versions/0025_product_recommendation_signals.py
+++ alembic/versions/0025_product_recommendation_signals.py
@@ -1,6 +1,6 @@
 """Add dedicated recommendation signals to products.
 
-Revision ID: 0025_product_recommendation_signals
+Revision ID: 0025_recommendation_signals
 Revises: 0024_dynamic_product_attributes
 """
 from typing import Sequence, Union
@@ -8,7 +8,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "0025_product_recommendation_signals"
+revision: str = "0025_recommendation_signals"
 down_revision: Union[str, None] = "0024_dynamic_product_attributes"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
